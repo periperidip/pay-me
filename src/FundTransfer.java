0f1207d0bd4cda4bf8d2feee4a0e158f10b6fa31
palashgarg0109 <plshgrg@gmail.com>
2020-10-21 15:03:17 +0530

    Created PIN-change and fund transfer file

---
+++ src/FundTransfer.java
@@ -0,0 +1,257 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+//import javax.swing.plaf.synth.SynthStyle;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import java.util.*;
+import java.sql.*;
+
+public class FundTransfer extends JFrame implements ActionListener{
+    JTextField toAccountNo,amount;
+    JPasswordField pin;
+    JButton cancel,transfer;
+    JLabel l_fund_transfer,l_toAccounNo,l_amount,l_pin;
+    JCheckBox showpassword;
+    String u_ID = "";
+    JPanel p_fund_transfer,p_toAccountNo,p_amount,p_pin,p_buttons,p_showpassword;
+
+    Border blackline = BorderFactory.createLineBorder(Color.black);
+    FundTransfer(){
+
+    }
+    FundTransfer(String User_ID){
+        u_ID = User_ID;
+
+        setTitle("Fund Transfer");
+		setSize(1200, 900);
+		setLocationRelativeTo(null);
+        setVisible(true);
+        
+        l_fund_transfer=new JLabel("TRANSFER MONEY");
+        l_fund_transfer.setFont(new Font("System",Font.BOLD,22));
+
+        l_toAccounNo= new JLabel("TO A/C #");
+        l_toAccounNo.setFont(new Font("System",Font.BOLD,22));
+
+        l_amount = new JLabel("AMOUNT");
+        l_amount.setFont(new Font("System",Font.BOLD,22));
+
+        l_pin = new JLabel("PIN");
+        l_pin.setFont( new Font("System",Font.BOLD,22));
+
+        toAccountNo = new JTextField(15);
+        toAccountNo.setFont(new Font("Raleway", Font.BOLD, 22));
+        
+        amount = new JTextField(15);
+        amount.setFont(new Font("Raleway", Font.BOLD, 22));
+        
+        pin = new JPasswordField(15);
+        pin.setFont(new Font("Raleway", Font.BOLD, 22));
+
+        transfer = new JButton("TRANSFER");
+        transfer.setFont(new Font("System", Font.BOLD, 18));
+        transfer.setBackground(Color.BLACK);
+        transfer.setForeground(Color.WHITE);
+    
+        cancel = new JButton("CANCEL");
+        cancel.setFont(new Font("System", Font.BOLD, 18));
+        cancel.setBackground(Color.BLACK);
+        cancel.setForeground(Color.WHITE);
+        
+        showpassword = new JCheckBox("Show Password");
+		showpassword.setFont(new Font("Serif", Font.BOLD, 20));
+		pin.setEchoChar('*');
+        
+        showpassword.addActionListener((ae) -> {
+			JCheckBox value = (JCheckBox) ae.getSource();
+            pin.setEchoChar(value.isSelected() ? (char) 0 : '*');
+        });
+        
+        p_fund_transfer = new JPanel();
+        p_toAccountNo = new JPanel();
+        p_amount = new JPanel();
+        p_pin = new JPanel();
+        p_buttons = new JPanel();
+        p_showpassword = new JPanel();
+
+        p_fund_transfer.setLayout(new BorderLayout());
+        p_toAccountNo.setLayout(new BorderLayout());
+        p_amount.setLayout(new BorderLayout());
+        p_pin.setLayout(new BorderLayout());
+        p_buttons.setLayout(new BorderLayout());
+        p_showpassword.setLayout(new BorderLayout());
+
+        GridLayout button_layout = new GridLayout(1, 3);
+		button_layout.setHgap(40);
+        p_buttons.setLayout(button_layout);
+        
+        p_toAccountNo.add(l_toAccounNo, BorderLayout.WEST);
+        p_toAccountNo.add(toAccountNo, BorderLayout.EAST);
+
+        p_amount.add(l_amount, BorderLayout.WEST);
+        p_amount.add(amount, BorderLayout.EAST);
+
+        p_pin.add(l_pin, BorderLayout.WEST);
+        p_pin.add(pin, BorderLayout.EAST);
+
+        p_showpassword.add(showpassword, BorderLayout.EAST);
+
+        p_fund_transfer.add(l_fund_transfer, BorderLayout.CENTER);
+        p_fund_transfer.setBorder(new EmptyBorder(40, 340, 0, 260));
+
+        p_buttons.add(transfer);
+        p_buttons.add(cancel);
+
+        GridLayout global_layout = new GridLayout(11, 1);
+		global_layout.setVgap(25);
+		JPanel global_panel = new JPanel(global_layout);
+        global_panel.setBorder(new EmptyBorder(60, 260, 100, 260));
+
+        global_panel.add(p_fund_transfer);
+        global_panel.add(p_toAccountNo);
+        global_panel.add(p_amount);
+        global_panel.add(p_pin);
+        global_panel.add(p_showpassword);
+        global_panel.add(p_buttons);
+        
+        
+
+        getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(p_fund_transfer, BorderLayout.NORTH);
+		getContentPane().add(global_panel, BorderLayout.CENTER);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+       
+        transfer.addActionListener(this);
+        cancel.addActionListener(this);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae){
+        try {
+
+            String a = toAccountNo.getText();
+            String b = amount.getText();
+            String c = new String(pin.getPassword());
+            long money = Long.parseLong(b);
+
+            if(ae.getSource()==transfer){
+                if(a.equals("")){
+                    JOptionPane.showMessageDialog(null, "Please enter the account no. to which you want to transfer money");
+                }
+                else if(b.equals("")){
+                    JOptionPane.showMessageDialog(null, "Please enter the amount you want to tranfer");
+                }
+                else if(c.equals("")){
+                    JOptionPane.showMessageDialog(null, "Please enter your pin number");
+                }
+
+                MysqlConnection c1 = new MysqlConnection();
+                ResultSet rs =null;
+                String getData = "select pin, balance, account_number from accounts where userID = '"+u_ID+"' ";
+                rs = c1.statement.executeQuery(getData);
+
+                String correct_pin = "";
+                long available_balance = 0;
+                String from_account_no = "";
+                while (rs.next()) {
+                    correct_pin =  (String) rs.getObject(1);
+                    available_balance = rs.getLong("balance");
+                    from_account_no = rs.getString("account_number");
+                }
+                //System.out.println(correct_pin + " " + available_balance + " " + a);
+
+                if(c.equals(correct_pin) ){
+                    if(available_balance < money){
+                        JOptionPane.showMessageDialog(null, "Not enough balance in your account");
+                    }
+                    else{
+                        MysqlConnection c2 = new MysqlConnection();
+                        ResultSet rs1 = null;
+                        long total_balance = -1; // current account balance to whom we are sending money;
+                        String check_user_exists = "select balance from accounts where account_number = '"+a+"' ";
+                        rs1 = c2.statement.executeQuery(check_user_exists);
+                        while (rs1.next()) {
+                            total_balance = rs1.getLong("balance");
+                            //System.out.println(total_balance);
+                        }
+                        
+                        if(total_balance >= 0){
+                              
+                            total_balance = total_balance + money;
+                            available_balance = available_balance - money;
+                            String q1 = "update accounts set balance = '"+available_balance+"' where userID = '"+u_ID+"' ";
+                            String q2 = "update accounts set balance =  '"+total_balance+"' where account_number = '"+a+"' ";
+                            c1.statement.executeUpdate(q1);
+                            c1.statement.executeUpdate(q2);
+
+                            String transaction_Id = getTransactionID();
+                            String to_account_no = a;
+                            long amount = money;
+                            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+                            String q3 = "insert into transaction values('"+transaction_Id+"','"+to_account_no+"','"+from_account_no+"','"+amount+"','"+timestamp+"')";
+                                        
+                            c1.statement.executeUpdate(q3);
+                            setVisible(false);
+                        }
+                        else{
+                            JOptionPane.showMessageDialog(null, "no user exists with given account no.");
+                        }
+                        
+                    }
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "Enter the correct PIN no.");
+                }
+            
+
+            }
+            else if(ae.getSource()==cancel){
+                setVisible(false);
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println("error in amount transfer");
+            System.out.println("error: "+e);
+        }
+    }
+
+    public static void main(String[] args){
+        new FundTransfer("90001").setVisible(true);
+    }
+
+    public static String increment(String str) {
+		int carry = 1;
+		int sum = 0;
+		String ret = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			sum = str.charAt(i) - '0' + carry;
+			carry = sum / 10;
+			sum = sum % 10;
+			ret = (char) (sum + '0') + ret;
+		}
+		if (carry != 0)
+			ret = (char) (carry) + ret;
+		return ret;
+    }
+    
+    public static String getTransactionID() throws SQLException {
+		MysqlConnection mysqlConnection = new MysqlConnection();
+
+		String check_empty = "SELECT transactionID from transaction";
+		ResultSet rs = mysqlConnection.statement.executeQuery(check_empty);
+
+		if (rs.next()) {
+			String max_transaction_id_query = "SELECT MAX(transactionID) FROM transaction";
+			ResultSet max_transaction_id_number = mysqlConnection.statement.executeQuery(max_transaction_id_query);
+			if (max_transaction_id_number.next())
+				return increment(max_transaction_id_number.getString(1));
+			else
+				return "15897840001";
+		} else
+			return "15897840001";
+	}
+}
---
+++ src/PIN_change.java
@@ -0,0 +1,219 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+//import javax.swing.plaf.synth.SynthStyle;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+import java.sql.*;
+
+public class PIN_change extends JFrame implements ActionListener{
+    JPasswordField current_pin,new_pin,confirm_pin;
+    JButton cancel,save;
+    JLabel l_change_pin,l_current_pin,l_new_pin,l_confirm_pin;
+    JPanel p_change_pin,p_current_pin,p_new_pin,p_confirm_pin,p_buttons,p_show_password;
+    JCheckBox show_password;
+    String u_ID = "";
+
+    Border blackline = BorderFactory.createLineBorder(Color.black);
+    public PIN_change(){
+
+    }
+
+    public PIN_change(String User_ID){
+
+        u_ID=User_ID;
+
+        setTitle("PIN Change");
+		setSize(1200, 900);
+		setLocationRelativeTo(null);
+		setVisible(true);
+
+        l_change_pin=new JLabel("CHANGE YOUR PIN");
+        l_change_pin.setFont(new Font("System",Font.BOLD,22));
+
+        l_current_pin= new JLabel("CURRENT PIN");
+        l_current_pin.setFont(new Font("System",Font.BOLD,22));
+
+        l_new_pin = new JLabel("NEW PIN");
+        l_new_pin.setFont(new Font("System",Font.BOLD,22));
+
+        l_confirm_pin = new JLabel("CONFIRM NEW PIN");
+        l_confirm_pin.setFont( new Font("System",Font.BOLD,22));
+
+        current_pin = new JPasswordField(15);
+        current_pin.setFont(new Font("Raleway", Font.BOLD, 22));
+        
+        new_pin = new JPasswordField(15);
+        new_pin.setFont(new Font("Raleway", Font.BOLD, 22));
+        
+        confirm_pin = new JPasswordField(15);
+        confirm_pin.setFont(new Font("Raleway", Font.BOLD, 22));
+        
+        save = new JButton("SAVE");
+        save.setFont(new Font("System", Font.BOLD, 18));
+        save.setBackground(Color.BLACK);
+        save.setForeground(Color.WHITE);
+    
+        cancel = new JButton("CANCEL");
+        cancel.setFont(new Font("System", Font.BOLD, 18));
+        cancel.setBackground(Color.BLACK);
+        cancel.setForeground(Color.WHITE);
+
+        show_password = new JCheckBox("Show Password");
+		show_password.setFont(new Font("Serif", Font.BOLD, 20));
+		current_pin.setEchoChar('*');
+        new_pin.setEchoChar('*');
+        confirm_pin.setEchoChar('*');
+
+        show_password.addActionListener((ae) -> {
+			JCheckBox value = (JCheckBox) ae.getSource();
+		    current_pin.setEchoChar(value.isSelected() ? (char) 0 : '*');
+            new_pin.setEchoChar(value.isSelected() ? (char) 0 : '*');
+            confirm_pin.setEchoChar(value.isSelected() ? (char) 0 : '*');
+		});
+
+        p_change_pin = new JPanel();
+        p_current_pin = new JPanel();
+        p_new_pin = new JPanel();
+        p_confirm_pin = new JPanel();
+        p_show_password = new JPanel();
+        p_buttons = new JPanel();
+
+        p_change_pin.setLayout(new BorderLayout());
+        p_current_pin.setLayout(new BorderLayout());
+        p_new_pin.setLayout(new BorderLayout());
+        p_confirm_pin.setLayout(new BorderLayout());
+        p_show_password.setLayout(new BorderLayout());
+        p_buttons.setLayout(new BorderLayout());
+       
+        GridLayout button_layout = new GridLayout(1, 3);
+		button_layout.setHgap(40);
+        p_buttons.setLayout(button_layout);
+        
+        p_current_pin.add(l_current_pin, BorderLayout.WEST);
+        p_current_pin.add(current_pin, BorderLayout.EAST);
+
+        p_new_pin.add(l_new_pin, BorderLayout.WEST);
+        p_new_pin.add(new_pin, BorderLayout.EAST);
+
+        p_confirm_pin.add(l_confirm_pin, BorderLayout.WEST);
+        p_confirm_pin.add(confirm_pin, BorderLayout.EAST);
+
+        
+        p_show_password.add(show_password, BorderLayout.EAST);
+
+        p_change_pin.add(l_change_pin, BorderLayout.CENTER);
+        p_change_pin.setBorder(new EmptyBorder(40, 340, 0, 260));
+        
+        p_buttons.add(save);
+        p_buttons.add(cancel);
+
+        GridLayout global_layout = new GridLayout(11, 1);
+		global_layout.setVgap(25);
+		JPanel global_panel = new JPanel(global_layout);
+        global_panel.setBorder(new EmptyBorder(60, 260, 100, 260));
+        
+
+        global_panel.add(p_change_pin);
+        global_panel.add(p_current_pin);
+        global_panel.add(p_new_pin);
+        global_panel.add(p_confirm_pin);
+        global_panel.add(p_show_password);
+        global_panel.add(p_buttons);
+
+
+        getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(p_change_pin, BorderLayout.NORTH);
+		getContentPane().add(global_panel, BorderLayout.CENTER);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+       
+        save.addActionListener(this);
+        cancel.addActionListener(this);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae){
+    
+        try{        
+            
+            String a = new String(current_pin.getPassword());
+            String b = new String(new_pin.getPassword());
+            String c = new String(confirm_pin.getPassword());
+            
+           // System.out.println(a + " "+b + " "+ c);
+            
+            if(ae.getSource()==save){
+                if(a.equals("")){                    
+                    JOptionPane.showMessageDialog(null, "Please Enter Current PIN");
+                }
+
+                else if(b.equals("")){
+                    JOptionPane.showMessageDialog(null, "Enter New PIN");
+                }
+
+                else if (c.equals("")){                    
+                    JOptionPane.showMessageDialog(null, "Re-Enter new PIN");
+                }
+                
+                else if(b.length() < 4){
+                    JOptionPane.showMessageDialog(null,"New PIN must be of four digits");
+                }
+
+                MysqlConnection c1 = new MysqlConnection();
+                ResultSet rs =null;
+                String getData = "select pin from accounts where userID = '"+u_ID+"' ";
+                rs = c1.statement.executeQuery(getData);
+                String output = "";
+                while (rs.next()) {
+                    output =  (String) rs.getObject(1);
+                }
+               // System.out.println(output);
+
+                if(a.equals(output)){
+
+                    if(b.equals(c)){
+                        
+                        // MysqlConnection c1 = new MysqlConnection();
+                         String q1 = "update accounts set pin = '"+b+"' where userID = '"+u_ID+"' ";
+                         
+                         c1.statement.executeUpdate(q1);
+                        
+                         JOptionPane.showMessageDialog(null, "PIN changed successfully");
+                         
+                         //new Transcations().setVisible(true);
+                         setVisible(false);
+                         
+                     }else{
+                         JOptionPane.showMessageDialog(null, "PIN entered doesn't match");
+                     }
+
+                }else{
+                    JOptionPane.showMessageDialog(null, "Please enter the correct current PIN");
+                }
+
+                    
+
+                
+            }else if(ae.getSource()==cancel){
+                
+               // new Transcations().setVisible(true);
+                setVisible(false);
+                
+            }
+            
+        }catch(Exception e){
+                    //e.printStackTrace();
+                    System.out.println("error in pin change");
+                    System.out.println("error: "+e);
+        }
+            
+    }
+
+    public static void main(String[] args){
+        new PIN_change("90001").setVisible(true);
+    }
+}
+
