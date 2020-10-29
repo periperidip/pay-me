9fee96583d87d18c6fff9d02fb5f7ae571e1a06d
Saksham <sakshamdam54321@gmail.com>
2020-10-29 11:16:31 +0530

    Removed reduntant imports and serialised classes

--- src/Feedback.java
+++ src/Feedback.java
@@ -7,11 +7,11 @@ import javax.swing.border.EmptyBorder;
 
 //import org.graalvm.compiler.lir.LIRInstruction.Use;
 
-import java.util.*;
 import java.sql.*;
 
 public class Feedback extends JFrame implements ActionListener{
     
+    private static final long serialVersionUID = 1L;
     JTextArea body_text, title_text;
     JButton cancel,submit;
     JLabel l_feedback, l_title, l_body;
